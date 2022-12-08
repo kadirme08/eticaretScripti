434a79592408e87aa02e8c65509f1a1da9af95d2
Kadir Can Koçak <kadirme0@gmail.com>
2022-12-08 23:54:44 +0300

    düzenleme yapıld

--- app/Http/Livewire/CategoryComponent.php
+++ app/Http/Livewire/CategoryComponent.php
@@ -21,9 +21,6 @@ class CategoryComponent extends Component
 
     public function store($product_id,$product_name,$product_price){
 
-
-
-
         Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Products');
         session()->flash('succses','Ürün ekleme İşlemi Başarılı');
         return redirect()->route('shop.cart');
